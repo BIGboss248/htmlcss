5d023b2988e791f36a383cab013b802788c5867a
Amin Jamali <53099680+BIGboss248@users.noreply.github.com>
2026-02-06 11:26:20 +0330

    Added button practice

---
+++ buttonPractice/main.js
@@ -0,0 +1,8 @@
+function buttonToggle() {
+  let buttonElement = document.querySelector(".finishButton");
+  if (buttonElement.innerHTML === "Finish") {
+    buttonElement.innerHTML = "Finished";
+  } else {
+    buttonElement.innerHTML = "Finish";
+  }
+}
