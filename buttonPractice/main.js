cacf4d40453d851a6d4945f66684e227c79d2077
Amin Jamali <53099680+BIGboss248@users.noreply.github.com>
2026-02-06 11:47:02 +0330

    Added styles

--- buttonPractice/main.js
+++ buttonPractice/main.js
@@ -1,8 +1,12 @@
 function buttonToggle() {
-  let buttonElement = document.querySelector(".finishButton");
+  let buttonElement = document.querySelector(".mainButton");
   if (buttonElement.innerHTML === "Finish") {
     buttonElement.innerHTML = "Finished";
+    buttonElement.classList.add("finishedButton");
+    buttonElement.classList.remove("finishButton");
   } else {
     buttonElement.innerHTML = "Finish";
+    buttonElement.classList.add("finishButton");
+    buttonElement.classList.remove("finishedButton");
   }
 }
